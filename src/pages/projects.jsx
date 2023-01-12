0246bc9656917b40e000dbfdec4cab19e90655a8
PeterKron <89381357+PeterKron@users.noreply.github.com>
2023-01-12 21:45:29 +0100

    some more styling in projects, and fixed categoryfunction

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -15,31 +15,101 @@ const ProjectsPage = ({ data }) => {
   const renderProjects = () => {
     if (currentCategory !== "") {
       return projects.nodes.map((project) => {
+        return project.category.map((cat) => {
+          if (cat.categoryName === currentCategory) {
+          return (
+            <article class="flex flex-col items-center justify-between border-solid border border-white bg-black rounded w-96 lg:m-4 mb-2 md:m-2 p-3">
+              <div>
+                <section class="flex items-center justify-between xs:self-stretch">
+                  <h3 class="py-1 text-2xl xs:text-3xl">
+                    {project.projectName}
+                  </h3>
+                  <Link to={project.projectLink.projectLink}>
+                    <h4 class="xs:text-lg">Go to project website</h4>
+                  </Link>
+                </section>
+                <section class="flex flex-col items-center justify-between mt-3">
+                <Link to={`/project/${project.slug}`}>
+                  <img
+                    class="w-full"
+                    src={project.screenshots[0].resize.src}
+                    alt=""
+                    />
+                    </Link>
+                  <p class="text-sm xs:text-base text-center mt-3">
+                    {renderRichText(project.projectDescription, options)}
+                  </p>
+                </section>
+              </div>
+              <Link to={`/project/${project.slug}`}>
+                <h4 class="xs:text-xl">Read more</h4>
+              </Link>
+            </article>
+          );
+        }
+      });
+    });
+  } else {
+    return projects.nodes.map((project) => (
+      <article class="flex flex-col items-center justify-between border-solid border border-white bg-black rounded w-96 lg:m-4 mb-2 md:m-2 p-3">
+        <div>
+          <section class="flex items-center justify-between xs:self-stretch">
+            <h3 class="py-1 text-2xl xs:text-3xl">{project.projectName}</h3>
+            <Link to={project.projectLink.projectLink}>
+              <h4 class="xs:text-lg">Go to project website</h4>
+            </Link>
+          </section>
+          <section class="flex flex-col items-center justify-between mt-3">
+          <Link to={`/project/${project.slug}`}>
+            <img
+              class="w-full"
+              src={project.screenshots[0].resize.src}
+              alt=""
+            />
+            </Link>
+            <p class="text-sm xs:text-base text-center mt-3">
+              {renderRichText(project.projectDescription, options)}
+            </p>
+          </section>
+        </div>
+        <Link to={`/project/${project.slug}`}>
+          <h4 class="xs:text-xl">Read more</h4>
+        </Link>
+      </article>
+    ));
+  }
+  };
+
+  const renderProjectsz = () => {
+    if (currentCategory !== "") {
+      return projects.nodes.map((project) => {
         if (project.category[0].categoryName === currentCategory) {
           return (
             <article class="flex flex-col items-center justify-between border-solid border border-white bg-black rounded w-96 lg:m-4  mb-2 md:m-2 p-3">
-            <div>
-              <section class="flex items-center justify-between xs:self-stretch">
-                <h3 class="py-1 text-2xl xs:text-3xl">{project.projectName}</h3>
-                <Link to={project.projectLink.projectLink}>
-                  <h4 class="xs:text-lg">Go to project website</h4>
-                </Link>
-              </section>
-              <section class="flex flex-col items-center justify-between mt-3">
-                <img
-                  class="w-full"
-                  src={project.screenshots[0].resize.src}
-                  alt=""
-                />
-                <p class="text-sm xs:text-base text-center mt-3">
-                  {renderRichText(project.projectDescription, options)}
-                </p>
-              </section>
-            </div>
-            <Link to={`/project/${project.slug}`}>
-              <h4 class="xs:text-xl">Read more</h4>
-            </Link>
-          </article>
+              <div>
+                <section class="flex items-center justify-between xs:self-stretch">
+                  <h3 class="py-1 text-2xl xs:text-3xl">
+                    {project.projectName}
+                  </h3>
+                  <Link to={project.projectLink.projectLink}>
+                    <h4 class="xs:text-lg">Go to project website</h4>
+                  </Link>
+                </section>
+                <section class="flex flex-col items-center justify-between mt-3">
+                  <img
+                    class="w-full"
+                    src={project.screenshots[0].resize.src}
+                    alt=""
+                  />
+                  <p class="text-sm xs:text-base text-center mt-3">
+                    {renderRichText(project.projectDescription, options)}
+                  </p>
+                </section>
+              </div>
+              <Link to={`/project/${project.slug}`}>
+                <h4 class="xs:text-xl">Read more</h4>
+              </Link>
+            </article>
           );
         }
       });
@@ -94,6 +164,7 @@ const ProjectsPage = ({ data }) => {
   return (
     <>
       <Header />
+
       <main class="p-2 ap:px-18 2xl:px-26 3xl:px-28 flex flex-col xs:pt-20 pt-16">
         <div class="flex justify-between pb-10 xs:justify-around xs:py-6 sm:pb-16">
           <h1 class="xs:text-5xl">My Projects</h1>
@@ -140,6 +211,7 @@ const ProjectsPage = ({ data }) => {
             )}
           </nav>
         </div>
+
         <div class="flex flex-wrap justify-center">{renderProjects()}</div>
       </main>
     </>
@@ -172,7 +244,7 @@ export const query = graphql`
         }
       }
     }
-    allContentfulCategory {
+    allContentfulCategory(sort: { categoryName: ASC }) {
       nodes {
         categoryName
       }
